99e67f2a71a0d6e9f1e6abb31f21af6c622db451
Youssef-Hammad <youssef88999@gmail.com>
2021-11-03 04:01:57 +0200

    Finished Texture class, added the GLenum for the texture as a static in the Texture class instead of passing it in the constructor like the previous renderer. Will continue on RenderableObject

--- Texture.cpp
+++ Texture.cpp
@@ -1,17 +1,48 @@
 #include "Texture.h"
 
-Texture::Texture()
+GLenum Texture::current_texture_number = GL_TEXTURE0;
+
+Texture::Texture(std::string path, glm::vec3 amb, glm::vec3 diff, glm::vec3 spec, float shine)
 {
+	material = { amb,diff,spec,shine };
+
+	glGenTextures(1, &ID);
+	texture_number = current_texture_number;
+
+	current_texture_number += 1;
+
+	glActiveTexture(texture_number);
+	glBindTexture(GL_TEXTURE_2D, ID);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* image_data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+	// if loaded successfully
+	if (image_data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, (nrChannels == 3 ? GL_RGB : GL_RGBA), GL_UNSIGNED_BYTE, image_data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load data" << std::endl;
+	}
+	stbi_image_free(image_data);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Texture::bind()
 {
+	glActiveTexture(texture_number);
+	glBindTexture(GL_TEXTURE_2D, ID);
 }
 
 void Texture::unbind()
 {
-}
-
-Texture::~Texture()
-{
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
--- Texture.h
+++ Texture.h
@@ -6,27 +6,30 @@
 #include <iostream>
 #include <glm/glm/glm.hpp>
 #include <glad/glad.h>
+#include "stb_image.h"
 
 struct Material
 {
 	glm::vec3 ambient;
 	glm::vec3 diffuse;
 	glm::vec3 specular;
+	float shininess;
 };
 
 class Texture
 {
+private:
+	static GLenum current_texture_number;
 public:
 	unsigned int ID;
 	std::string path;
+	GLenum texture_number;
 
 	Material material;
 
-	Texture();
+	Texture(std::string path,glm::vec3 amb, glm::vec3 diff, glm::vec3 spec, float shine);
 	void bind();
 	void unbind();
-
-	~Texture();
 };
 
 #endif // !TEXTURE_H
